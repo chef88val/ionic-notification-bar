566a46946b95b1b30421cffaa0d80615b9462d7c
Guillaume Emery <gemery@outlook.fr>
2016-10-10 10:05:21 +0200

    Transfert on github

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp');
+var minify = require('gulp-minify');
+var minifyCss = require('gulp-minify-css');
+
+gulp.task('minify', function() {
+    gulp.src('src/*.js')
+        .pipe(minify())
+        .pipe(gulp.dest('./dist/'));
+
+    gulp.src('src/*.css')
+        .pipe(minifyCss({
+            keepSpecialComments: 0
+        }))
+        .pipe(gulp.dest('./dist/'));
+});
